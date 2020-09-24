4475bd285c1eb734cabf5a98a253edbc210033d7
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-09-24 15:49:20 -0400

    iterate through survivors array and render a Survivor component to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import survivors from './survivors'
+
+const Survivor = ({ survivor }) => (
+  <div>
+    <img src={survivor.avatar} alt={survivor.name} />
+    <p>{survivor.name}</p>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
       <h1>Risk of Rain 2 Character Builder</h1>
+      <p>To get started, choose a survivor below.</p>
+      <ul>
+        {survivors.map(survivor => <Survivor key={survivor.id} survivor={survivor} />)}
+      </ul>
     </div>
   );
 }
