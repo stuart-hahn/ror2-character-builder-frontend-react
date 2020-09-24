78c0b2284703598cc85493ad813ddd5192e406cf
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-09-24 15:48:48 -0400

    separate array of survivor objects into its own file

---
+++ src/survivors.js
@@ -0,0 +1,54 @@
+const survivors = [
+  {
+    id: '0',
+    name: "Acrid",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/8/8e/Acrid.png"
+  },
+  {
+    id: '1',
+    name: "Artificer",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/9/98/Artificer.png"
+  },
+  {
+    id: '2',
+    name: "Captain",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/a/a9/Captain.png"
+  },
+  {
+    id: '3',
+    name: "Commando",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/5/50/Commando.png"
+  },
+  {
+    id: '4',
+    name: "Engineer",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/d/d8/Engineer.png"
+  },
+  {
+    id: '5',
+    name: "Huntress",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/6/6f/Huntress.png"
+  },
+  {
+    id: '6',
+    name: "Loader",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/2/25/Loader.png"
+  },
+  {
+    id: '7',
+    name: "Mercenary",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/0/00/Mercenary.png"
+  },
+  {
+    id: '8',
+    name: "MUL-T",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/6/6f/MUL-T.png"
+  },
+  {
+    id: '9',
+    name: "Rex",
+    avatar: "https://static.wikia.nocookie.net/riskofrain2_gamepedia_en/images/7/74/REX.png"
+  },
+]
+
+export default survivors
