a19b82e16e994a5a56badd12652ef93f78064952
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-09-25 15:28:17 -0400

    style the InputWithLabel component

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => (
-  <div>
-    <label htmlFor={id}>{label}: </label>
-    <input id={id} type={type} onChange={onInputChange} value={value} />
+  <div className="ui icon input">
+    <input id={id} type={type} onChange={onInputChange} value={value} placeholder={`${label}...`} />
+    <i className="search icon"></i>
   </div>
 )
 
